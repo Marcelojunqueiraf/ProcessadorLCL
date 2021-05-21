8da16d768d9b5740fb46432c9fa87081dcb372c6
Marcelo <marcelojunqueiraf@gmail.com>
2021-05-20 23:21:09 -0300

    Bloco PC Bloco ULA e Novo bloco de controle

---
+++ Processador/programar/ROMGen.py
@@ -0,0 +1,19 @@
+
+opCode = {"addi":"0000", "subi":"0001", "andi":"0010", "oi":"0011", "xoi":"0100", "beq":"0101", "bleu":"0110", "bles":"0111"}
+opCode = {"addi":"0000", "subi":"0001", "andi":"0010", "oi":"0011", "xoi":"0100", "beq":"0101", "bleu":"0110", "bles":"0111"}
+
+while True:
+	inst = input()
+	if inst == "stop":
+		break
+	inst = inst.replace("r", "").replace(",","")
+	inst = inst.split()
+	op = opCode[inst[0]]
+	rd = int(inst[1])
+	ra = int(inst[2])
+	rb = int(inst[3])
+	imm = int(inst[4])
+	print("op:"+op, "ra:"+str(ra), "rb:"+str(rb), "rd:"+str(rd), "imm:"+str(imm))
+	result =  hex(rd)[2:] + hex(ra)[2:].zfill(4) + rex(rb)[2:].zfill(4) + op
+	print(result)
+	print(bin(imm)[2:].zfill(16))
